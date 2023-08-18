f4cd63f7070f6a6a450ffc2924baa75a33ed11b1
Joann <91803627+JoanneGamer@users.noreply.github.com>
2023-08-18 17:22:30 -0600

    index.js

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const { AoiClient, LoadCommands } = require("aoi.js");
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const fs = require('fs');
+///Configuramos para que reconozca que valla todo en la carpeta "src" y a la carpeta "config"
 const config = require("./src/config");
 
 const bot = new AoiClient({
+    ///decimos que el token, prefix, los intents y eventos, va a estar en la carpeta "config"
     token: config.token,
     prefix: config.prefix,
     intents: config.intents,
     events: config.events, 
+    ///se asigna una base de datos a aoi.mongo
     database: {
         type: "aoi.mongo",
         db: require("aoi.mongo"),
         tables: ["main"],
+        ///Link de su base de datos
         path: "",
     }    
 });
+///Nuevamente el link
 mongoose.connect('', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -23,6 +28,7 @@ mongoose.connect('', {
 mongoose.connection.once('open', () => {  console.log('Conexión a MongoDB establecida correctamente.');
 });
 
+////Sistema de anticrash, su funcion es para que no se apague mientras algo salio mal
 //Anticrash
 const files = fs.readdirSync('./src/events').filter(file => file.endsWith('js'))
 files.forEach( x => {
